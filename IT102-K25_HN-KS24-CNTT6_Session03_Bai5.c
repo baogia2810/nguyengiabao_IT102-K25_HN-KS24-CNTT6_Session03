a0d3bd4da67e8809ff6602393f427b83ced1cd2a
baogia2810 <bankaijun2880@gmail.com>
2025-07-30 09:02:32 +0700

    upload bài 5

---
+++ IT102-K25_HN-KS24-CNTT6_Session03_Bai5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("Danh sach sinh vien:\n");
+    printf("1. Nguyen Van A - 20 - 0964484821 - amg@rikikelaacademy.com\n");
+    printf("2. Nguyen Van B - 21 - 0964484822 - bmg@rikikelaacademy.com\n");
+    printf("3. Nguyen Van C - 18 - 0964484823 - cmg@rikikelaacademy.com\n");
+    printf("4. Nguyen Van D - 19 - 0964484824 - dmg@rikikelaacademy.com\n");
+    printf("5. Nguyen Van E - 22 - 0964484825 - emg@rikikelaacademy.com\n");
+    printf("6. Nguyen Van F - 23 - 0964484826 - fmg@rikikelaacademy.com\n");
+    printf("7. Nguyen Van G - 23 - 0964484827 - gmg@rikikelaacademy.com\n");
+    printf("8. Nguyen Van H - 20 - 0964484828 - hmg@rikikelaacademy.com\n");
+    printf("9. Nguyen Van I - 18 - 0964484829 - img@rikikelaacademy.com\n");
+    printf("10. Nguyen Van K - 21 - 0964484830 - kmg@rikikelaacademy.com\n");
+    return 0;
+}
